6878142e4b972fbf3adbe1ba70dd407a85de47f7
Yuniarrr <5027211025@mhs.its.ac.id>
2024-02-12 16:39:01 +0700

    feat: initial service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { PrismaService } from 'infra/database/prisma/prisma.service';
 import { AuthModule } from './services/auth/auth.module';
 import { NotesModule } from './services/notes/notes.module';
 import { UsersModule } from './services/users/users.module';
+import { NotesModule } from './notes/notes.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [CacheModule.register(), AuthModule, NotesModule, UsersModule],
--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
