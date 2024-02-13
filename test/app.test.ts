b947268c19e38e8ff60d2ce52637c4ae9a9b4e64
Yuniarrr <5027211025@mhs.its.ac.id>
2024-02-13 15:00:11 +0700

    feat: e2e testing

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,36 +1,56 @@
-import { type INestApplication } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { type INestApplication, HttpStatus } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { type TestingModule, Test } from '@nestjs/testing';
 
 import request from 'supertest';
 
 import { AppModule } from '../src/app.module';
-import { PrismaService } from '../src/infra/database/prisma/prisma.service';
 
-describe('AppController (e2e)', () => {
+describe('Token API (End-to-End)', () => {
   let app: INestApplication;
+  let jwtService: JwtService;
+  let authToken: string;
 
-  const mockPrismaService = {
-    user: {
-      findMany: jest.fn().mockResolvedValue([]),
-      findUnique: jest.fn().mockResolvedValue({}),
-      update: jest.fn().mockResolvedValue({}),
-      create: jest.fn().mockResolvedValue({}),
-      delete: jest.fn().mockResolvedValue({}),
-    },
-  };
-
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      .overrideProvider(PrismaService)
-      .useValue(mockPrismaService)
-      .compile();
+    }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    jwtService = moduleFixture.get<JwtService>(JwtService);
+
+    const user_id = 'febb25f9-008c-452b-aacf-e15f053c0373';
+    const email = 'example@gmail.com';
+    const payload = {
+      user_id,
+      email,
+    };
+    authToken = jwtService.sign(payload);
   });
 
-  it('/ (GET)', () =>
-    request(app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should return 401 if token is missing', () =>
+    request(app.getHttpServer())
+      .get('/category')
+      .expect(HttpStatus.UNAUTHORIZED));
+
+  it('should return 401 if token is invalid', () =>
+    request(app.getHttpServer())
+      .get('/category')
+      .set('Authorization', 'Bearer invalid_token')
+      .expect(HttpStatus.UNAUTHORIZED));
+
+  it('should return 200 if token is valid', () =>
+    request(app.getHttpServer())
+      .get('/category')
+      .set('Authorization', `Bearer ${authToken}`)
+      .expect(HttpStatus.OK));
 });
